bb4562708ce7b40c84c773275bf8c4caa38074aa
Michael Antwi <60970495+mikeosei5177@users.noreply.github.com>
2026-04-10 11:18:38 +0100

    Upgrade AWS provider to version 6.40.0
    Updated AWS provider version and region.

--- EKS_TERRAFORM/provider.tf
+++ EKS_TERRAFORM/provider.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "6.40.0"
     }
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
